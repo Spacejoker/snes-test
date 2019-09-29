f292e3b0f570471520d7721a1e2bdec92a918375
js <stahl.jens@gmail.com>
2019-09-29 16:43:19 -0700

    Cleanup.

--- Jens.asm
+++ Jens.asm
@@ -1,6 +1,3 @@
- ; SNES Initialization Tutorial code
- ; This code is in the public domain.
- 
  .include "Header.inc"
  .include "Snes_Init.asm"
  
@@ -15,9 +12,9 @@
  Start:
      Snes_Init
  
-     ; Set the background color to green.
+     ; Set the background color to Red.
      sep     #$20        ; Set the A register to 8-bit.
-     lda     #%10000000  ; Force VBlank by turning off the screen.
+     lda     #$80  ; Force VBlank by turning off the screen.
      ; $2100 Screen Display Register, eight bit forces VBlank.
      sta     $2100
      ; Color format is 0BBBBBGG|GGGRRRRR, one byte at a time (low first)
@@ -27,7 +24,7 @@
      lda     #$00  ; Load the high byte of the color.
      sta     $2122
      ; Same register as before, but set brightness to 15 (100%).
-     lda     #%00001111
+     lda     #$0F
      sta     $2100
  
  Loop:
