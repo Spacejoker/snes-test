f918c6db53e3640cd9003295efe79160799cec87
js <stahl.jens@gmail.com>
2019-10-01 20:18:34 -0700

    Test some SNES asm based on a tutorial.

--- tic-tac-toe-init/Main.asm
+++ tic-tac-toe-init/Main.asm
@@ -1,3 +1,4 @@
+;Testing stuff based on this tutorial: https://wiki.superfamicom.org/making-a-small-game-tic-tac-toe
 .include "header.inc"
 .include "InitSNES.asm"
 
@@ -17,17 +18,90 @@ rti
 ;--------------------------------------
 Start:
   InitSNES
-sep #$30        ; get 8-bit registers
-stz $2121       ; write to CGRAM from $0
-lda #%11101111  ; this is
-ldx #%00111111  ; a green color
-sta $2122       ; write it
-stx $2122       ; to CGRAM
-lda #%00001111  ; turn on screen
-sta $2100       ; here
 
+rep #%00010000  ;16 bit xy
+sep #%00100000  ;8 bit ab
+
+; Palette loading (I don't understand this)
+; repeat 8 times, lda
+ldx #$0000
+-: lda UntitledPalette.l, x
+sta $2122
+inx
+cpx #8
+; branch unless x is 8
+bne -
+
+ldx #UntitledData   ; Address
+lda #:UntitledData  ; of UntitledData
+ldy #(15*16*2)      ; length of data
+stx $4302           ; write
+sta $4304           ; address
+sty $4305           ; and length
+lda #%00000001      ; set this mode (transferring words)
+sta $4300
+lda #$18            ; $211[89]: VRAM data write
+sta $4301           ; set destination
+
+ldy #$0000          ; Write to VRAM from $0000
+sty $2116
+
+lda #%00000001      ; start DMA, channel 0
+sta $420B
+
+lda #%10000000	; VRAM writing mode
+sta $2115
+ldx #$4000	    ; write to vram
+stx $2116       ; from $4000
+; End palette magic
+
+; My nice checker pattern of + signs.
+.rept 8
+  .rept 16
+    ldx #$0006
+    stx $2118
+    ldx #$0000
+    stx $2118
+  .endr
+  .rept 16
+    ldx #$0000
+    stx $2118
+    ldx #$0006
+    stx $2118
+  .endr
+.endr
+
+;set up the screen & modes
+lda #%00110000  ; 16x16 tiles, mode 0
+sta $2105       ; screen mode register
+lda #%01000000  ; data starts from $4000
+sta $2107       ; for BG1
+lda #%01100000  ; and $6000
+sta $2108       ; for BG2
+
+stz $210B	    ; BG1 and BG2 use the $0000 tiles
+
+lda #%00000001  ; enable bg1 only (not bg2)
+sta $212C
+
+lda #%00001111  ; enable screen, set brightness to 15
+sta $2100
+
+lda #%10000001  ; enable NMI and joypads
+sta $4200
+
+; Game loop
 forever:
 wai
 jmp forever
+
 ;--------------------------------------
+
+.ends
+
+; Static content
+.bank 1 slot 0
+.org 0
+.section "Tiledata"
+.include "tiles.inc"
 .ends
